4c00380b54b988d39cceb9a4767688cbb49ed6ea
Fatih BARAÇKILIÇ <fatihb469@gmail.com>
2025-07-30 09:59:47 +0300

    test(1342): add missing test case for numberOfSteps

--- src/tests/01342-number-of-steps-to-reduce-a-number-to-zero.test.ts
+++ src/tests/01342-number-of-steps-to-reduce-a-number-to-zero.test.ts
@@ -2,6 +2,7 @@ import { describe, expect, test } from "vitest";
 import {
   numberOfSteps,
   numberOfStepsBitManipulation,
+  numberOfStepsBitManipulation2,
 } from "../01342-number-of-steps-to-reduce-a-number-to-zero";
 
 describe("01342 numberOfSteps", () => {
@@ -23,5 +24,16 @@ describe("01342 numberOfStepsBitManipulation", () => {
     [0, 0],
   ])("should return %i steps for number %i", (excepted, num) => {
     expect(numberOfStepsBitManipulation(num)).toBe(excepted);
+  });
+});
+
+describe("01342 numberOfStepsBitManipulation", () => {
+  test.each([
+    [6, 14],
+    [4, 8],
+    [12, 123],
+    [0, 0],
+  ])("should return %i steps for number %i", (excepted, num) => {
+    expect(numberOfStepsBitManipulation2(num)).toBe(excepted);
   });
 });
